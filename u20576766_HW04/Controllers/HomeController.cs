4e1ce391c98f6a531fd2f60e5a6df58afbbf2b07
u20576766 <u20576766@tuks.co.za>
2022-08-20 09:46:07 +0200

    Editing the adopt page, home page and about page.

--- u20576766_HW04/Controllers/HomeController.cs
+++ u20576766_HW04/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using u20576766_HW04.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.IO;
 
 namespace u20576766_HW04.Controllers
 {
@@ -13,5 +15,15 @@ namespace u20576766_HW04.Controllers
             return View();
         }
 
+        public ActionResult Adopt()
+        {
+            string[] fileDirectory = Directory.GetFiles(Server.MapPath("~/Media/Adopt/"));
+            List<Filemedia> files = new List<Filemedia>();
+            foreach (string filePath in fileDirectory)
+            {
+                files.Add(new Filemedia { FileName = Path.GetFileName(filePath) });
+            }
+            return View(files);
+        }
     }
 }
---
+++ u20576766_HW04/Models/Filemedia.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+
+namespace u20576766_HW04.Models
+{
+    public class Filemedia
+    {
+        public string FileName { get; set; }
+    }
+}
